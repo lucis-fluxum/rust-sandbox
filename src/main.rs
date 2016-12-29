1bd3e7b052f48e2dee52c2851fe5ea1ad7e8dad3
Luc Street <lucis-fluxum@users.noreply.github.com>
2016-12-29 00:50:33 -0800

    Finish number guessing game tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    let secret_num = rand::thread_rng().gen_range(0, 100) + 1;
+
+    // println!("The secret number is {}", secret_num);
+
+    loop {
+        println!("Enter a guess: ");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        print!("{} is ", guess);
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("too small."),
+            Ordering::Greater => println!("too big."),
+            Ordering::Equal => {
+                println!("correct!");
+                break;
+            }
+        }
+    }
 }
