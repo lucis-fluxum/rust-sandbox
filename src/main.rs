70d09e6561797a2b30de6704abcc804f6b1aa30d
Luc Street <lucis-fluxum@users.noreply.github.com>
2017-03-06 20:21:06 -0800

    Implement traits on arbitrary types

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-use std::collections::HashMap;
-use std::io;
+trait SpicyMeatball {
+    fn get_msg(&self) -> String;
+}
+
+impl SpicyMeatball for bool {
+    fn get_msg(&self) -> String {
+        if *self {
+            String::from("mamma mia")
+        } else {
+            String::from("that's a spicy meatball")
+        }
+    }
+}
+
+impl SpicyMeatball for i32 {
+    fn get_msg(&self) -> String {
+        format!("mamma mia, that meatball is {} times too spicy", *self)
+    }
+}
 
 fn main() {
-    let mut roles = HashMap::new();
-
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).expect("Nope");
-
-    for entry in buffer.split(',') {
-        let mut parts_iterator = entry.split(':')
-            .map(str::trim)
-            .map(str::to_string);
-
-        roles.insert(parts_iterator.next().unwrap(),
-                     parts_iterator.next().unwrap());
-    }
-
-    println!("{:#?}", roles);
+    println!("{}, {}", true.get_msg(), false.get_msg());
+    println!("{}", 25.get_msg());
 }
